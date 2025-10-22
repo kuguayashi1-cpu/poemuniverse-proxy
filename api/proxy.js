d995c1e233926e7803fc00fc6bd63e27f689c1ff
kuguayashi1-cpu <kuguayashi1@gmail.com>
2025-10-22 14:41:37 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
-    const targetUrl = "https://api.openai.com/v1/chat/completions";
-    const apiKey = process.env.OPENAI_API_KEY;
+    const targetUrl = "https://api.moonshot.cn/v1";
+    const apiKey = process.env.sk-oUEqa6Zt6Ycd0x7f0vfsCodSBeRD75UeoPwfaJPBBov81JlA;
   
     const response = await fetch(targetUrl, {
       method: "POST",
